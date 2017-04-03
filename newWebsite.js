7a0c1b777169a66532ce07cc397169ae3c097779
Jazilch <zilchjames5@gmail.com>
2017-04-03 10:32:12 -0400

    Travel Berlin
    Guide To Traveling Berlin

---
+++ newWebsite.js
@@ -0,0 +1,11 @@
+
+
+$(document).ready(function(){
+	$("h1").hide();
+	$("h1").fadeIn(3000);
+});
+
+$(document).ready(function(){
+	$(".hero-setion, h2").hide();
+	$(".hero-setion, h2").fadeIn(7000);
+});
